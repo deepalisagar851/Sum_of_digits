785c70f3056c91ea89904bf22948bb43cc7583f8
Deepali <96642348+deepalisagar851@users.noreply.github.com>
2022-04-08 14:23:27 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Sum_of_digits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input an Integer:");
+            int x = Convert.ToInt32(Console.ReadLine());
+            int sum = 0;
+            while(x!=0)
+            {
+              
+                sum = sum+x % 10;
+                x = x / 10;
+            }
+            Console.WriteLine("The sum of digits is:"+sum);
+        }
+    }
+}
